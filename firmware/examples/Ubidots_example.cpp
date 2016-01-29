a92ec53e6649853112513e680695f4fa9e1753e6
Mateo Velez <metavixmv@gmail.com>
2016-01-29 18:59:37 -0500

    Update Ubidots_example.cpp

--- firmware/examples/Ubidots_example.cpp
+++ firmware/examples/Ubidots_example.cpp
@@ -8,16 +8,26 @@ void setup() {
     Serial.begin(115200);
 }
 void loop() {
-    //char*  pch = Ubidots.get_or_create_datasource();
-    //char* pch2 = Ubidots.get_or_create_variable(pch, "temperatura");
-    /*UbidotsCollection *coll = Ubidots.ubidots_collection_init(3);
-    Ubidots.ubidots_collection_add(coll, "56829c367625424b83d95d86", 23);
-    Ubidots.ubidots_collection_add(coll, "56829c2f7625424c4a4752af", 5);
-    Ubidots.ubidots_collection_add(coll, "5681ab677625423c3c26f90e", 32);
-    Ubidots.ubidots_collection_save(coll);
-    Ubidots.ubidots_collection_cleanup(coll);*/
-    
-    Ubidots.send_ubidots( 3, "Temperatura",2.3,"Servomotor", 3.2, "Humedad", 4.2 );
-    
+    #include "application.h"
+#include "Ubidots.h"
 
+#define TOKEN "YOUR_TOKEN_OF_UBIDOTS_HERE"
+
+#define ID "YOUR_ID_HERE" 
+
+Ubidots ubidots(TOKEN);
+
+void setup() {
+    Serial.begin(115200);
+    
+}
+void loop() {
+    
+    float value1 = analogRead(A0);
+    float value2 = analogRead(A1);
+    float value3 = analogRead(A2);
+    ubidots.send_ubidots( value1, "Temperatura", ram,"Servomotor", value2, "Humedad", value3 );
+    float n = ubidots.get_ubidots(ID);
+    Serial.println(n);
+    delay(300);
 }
