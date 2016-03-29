74f4fdafe95517873e8a2ff0b79109a9061d1edc
metavix <metavix@localhost.localdomain>
2016-03-29 18:37:07 -0500

    A new library with translate method, is simpler than old library

--- firmware/Ubidots.h
+++ firmware/Ubidots.h
@@ -1,58 +1,38 @@
-#ifndef __Ubidots_H_
-#define __Ubidots_H_
-#define DEBUG_UBIDOTS
+#ifndef _Ubidots_H_
+#define _Ubidots_H_
 
 #include "application.h"
 #include "spark_wiring_string.h"
 #include "spark_wiring_tcpclient.h"
 #include "spark_wiring_usbserial.h"
-#include <stdarg.h>
 
-#define USER_AGENT  "Ubidots/v1 Particle/1.0"
-#define BASE_URL  "things.ubidots.com"
-#define ATTEMPS  3
-#define BUFFER_HTTP_SIZE 1500
-
+#define SERVER "translate.ubidots.com"
+#define PORT 9010
 
 typedef struct Value {
-    char *name;
-    float value;
-    char *id;
-    Value *next;
+  char  *idName;
+  char  *contextOne;
+  char  *contextTwo;
+  float idValue;
 } Value;
 
-typedef struct UbidotsCollection {
-    char * idDatasourceDefault;
-    Value *first;
-} UbidotsCollection;
-
-
 class Ubidots {
- private:
-    TCPClient _client;
-    UbidotsCollection * cache;
-    char* pId;
-    char* _token;
-    char* dsName;
-
-    Value * checkInitValue(UbidotsCollection *collection, char* name, double value, char * id);
-    char* assemble(char* method, char* endpoint);
-    char* assembleWithData(char* method, char* endpoint, char* data);
-    char* parserId(char* status, char* body);    
-    char* parserValue(char* status, char* body);
-    
- public:
-    Ubidots(char* token);
-    Ubidots(char* token, char* datasourceName);
-    char* getOrCreateDatasource(char* ds_name);
-    char* getOrCreateVariable(char* dsId, char* variableName);
-    bool send(char* chain, char* status, char* body, unsigned int size);
-    bool sendWithReconnect(char* chain, char* status, char* body, unsigned int size);        
-    int ubidotsCollectionSave(UbidotsCollection *collection);        
-    void addValueWithName(UbidotsCollection *collection, char * name, double value);
-    void addValue(UbidotsCollection *collection, char * variableId, double value);    
-    bool sendUbidots(int number, ... );
-    float getUbidotsValue(char* id);
-    
+   public:
+      Ubidots(char* token);
+      bool sendAll();
+      bool sendAll(char* dsName);
+      float getValue(char* id);
+      void add(char *variable_id, double value);
+      void add(char *variable_id, double value, char *ctext1);
+      void add(char *variable_id, double value, char *ctext1, char *ctext2);
+      float getValueWithDatasource(char* dsName, char* idName);
+   
+   private:
+      TCPClient _client;
+      char* _token;
+      uint8_t maxValues;
+      uint8_t currentValue;
+      Value * val;
 };
+
 #endif
--- firmware/examples/UbidotsGetValue.cpp
+++ firmware/examples/UbidotsGetValue.cpp
@@ -1,16 +1,17 @@
-// This example is to get a value of variable from the Ubidots API
+// This example is to get the last value of variable from the Ubidots API
 
 #include "Ubidots.h"
 
-#define TOKEN "Your_token_here"
-#define ID "Your_ID_here"
+#define TOKEN "Your_token_here"  // Put here your Ubidots TOKEN
+#define DATASOURCENAME "Your_datasource_name"  // Put here your Ubidots datasource name 
 
-Ubidots ubidots(TOKEN);
-
+Ubidots ubidots(TOKEN, DATASOURCENAME);
 
 void setup() {
     Serial.begin(115200);
 }
 void loop() {    
+    float value;
+    value = ubidots.getValueWithDatasource(DATASOURCENAME, "Name_of_your_variable");
     ubidots.getUbidotsValue(ID);
 }
