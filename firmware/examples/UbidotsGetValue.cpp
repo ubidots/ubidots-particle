131b3a83dec9cd58cb5b81be79231f1117575202
metavix <metavix@localhost.localdomain>
2016-03-30 17:18:48 -0500

    fixed ident of examples

--- firmware/examples/UbidotsGetValue.cpp
+++ firmware/examples/UbidotsGetValue.cpp
@@ -3,14 +3,14 @@
 #include "Ubidots.h"
 
 #define TOKEN "Your_token_here"  // Put here your Ubidots TOKEN
-#define DATASOURCENAME "Your_datasource_name"  // Put here your Ubidots datasource name 
+#define DATASOURCENAME "Your_datasource_name"  // Put here your Ubidots datasource name
 
 Ubidots ubidots(TOKEN, DATASOURCENAME);
 
 void setup() {
     Serial.begin(115200);
 }
-void loop() {    
+void loop() {
     float value;
     value = ubidots.getValueWithDatasource(DATASOURCENAME, "Name_of_your_variable");
     ubidots.getUbidotsValue(ID);
--- firmware/examples/UbidotsSendValues.cpp
+++ firmware/examples/UbidotsSendValues.cpp
@@ -4,21 +4,20 @@
 
 
 #define TOKEN "Your_token_here"  // Put here your Ubidots TOKEN
-#define DATASOURCENAME "Your_datasource_name"  // Put here your Ubidots datasource name 
+#define DATASOURCENAME "Your_datasource_name"  // Put here your Ubidots datasource name
 
 Ubidots ubidots(TOKEN, DATASOURCENAME);
 
-void setup(){
-	Serial.begin(115200);
+void setup() {
+    Serial.begin(115200);
 }
-
-void loop(){
-	float value1 = analogRead(A0);
-	float value2 = analogRead(A1);
-	float value3 = analogRead(A2); 
-	ubidots.add("Variable_name_one", value1);  // Change for your variable name
-        ubidots.add("Variable_name_two", value2);
-        ubidots.add("Variable_name_three", value3);
-	ubidots.sendAll();
-	delay(60000);  // Delay of one minute
+void loop() {
+    float value1 = analogRead(A0);
+    float value2 = analogRead(A1);
+    float value3 = analogRead(A2);
+    ubidots.add("Variable_name_one", value1);  // Change for your variable name
+    ubidots.add("Variable_name_two", value2);
+    ubidots.add("Variable_name_three", value3);
+    ubidots.sendAll();
+    delay(60000);  // Delay of one minute
 }
