c0ea1095014d4052ea7654b36373a43adf556559
Jose Garcia <jose.reyes@ubidots.com>
2017-01-06 13:09:03 -0500

    added function for casting long to char*

--- firmware/examples/UbidotsSendValuesWithTimeStamp.cpp
+++ firmware/examples/UbidotsSendValuesWithTimeStamp.cpp
@@ -1,6 +1,6 @@
 // This example is to save multiple variables to the Ubidots API with TCP method
 
-#include "Ubidots.h"
+#include "Ubidots/Ubidots.h"
 
 
 #define TOKEN "Your Ubidots TOKEN"  // Put here your Ubidots TOKEN
@@ -12,7 +12,13 @@ void setup() {
 }
 void loop() {
     float value1 = analogRead(A0);
-    ubidots.add("test", value1, "", "1451518741000");  // Change for your variable name
+    long t = 1451518741L;
+    char* timestamp = (char *) malloc(sizeof(char) * 700);
+    sprintf(timestamp, "%d%s", t, "000");
+    Serial.print("timestamp:");
+    Serial.println(t);
+    ubidots.add("test", value1, NULL, timestamp);  // Change for your variable name
     ubidots.sendAll();
-    delay(5000);
+    timestamp = 0L;
+    delay(10000);
 }
