f3f78a906227c03e304a85b9af615fb10436877a
Jose Garcia <jota@localhost.localdomain>
2018-11-16 09:32:18 -0500

    Adding additional examples

---
+++ examples/SendContext/SendContext.cpp
@@ -0,0 +1,53 @@
+// This example is to get the last value of variable from the Ubidots API
+
+/****************************************
+ * Include Libraries
+ ****************************************/
+
+#include "Ubidots.h"
+
+/****************************************
+ * Define Constants
+ ****************************************/
+
+#ifndef UBIDOTS_TOKEN
+#define UBIDOTS_TOKEN ""  // Put here your Ubidots TOKEN
+#endif
+
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+
+//Put here your auxiliar functions
+
+
+/****************************************
+ * Main Functions
+ ****************************************/
+
+
+Ubidots ubidots(UBIDOTS_TOKEN);
+
+void setup() {
+    Serial.begin(115200);
+    //ubidots.setDebug(true); //Uncomment this line for printing debug messages
+}
+void loop() {
+    float value = analogRead(A0);
+
+    /* Adds context key-value pairs */
+    ubidots.addContext("weather-status", "sunny");
+    ubidots.addContext("time", "11:40:56 pm");
+
+    /* Builds the context with the coordinates to send to Ubidots */
+    ubidots.getContext(context);
+
+    /* Sends the position */
+    ubidots.add("temperature", value, context);  // Change for your variable name
+
+    if (ubidots.send("weather-station")) {  // Sends position to a device with label that matches the Particle Device Id
+      Serial.println("Position sent");
+    }
+    
+    delay(5000);
+}
---
+++ examples/SendValuesParticleWebhook/SendValuesParticleWebhook.cpp
@@ -0,0 +1,59 @@
+// This example is to get the last value of variable from the Ubidots API
+
+// This example is to save multiple variables to the Ubidots API with TCP method
+
+/****************************************
+ * Include Libraries
+ ****************************************/
+
+#include "Ubidots.h"
+
+/****************************************
+ * Define Constants
+ ****************************************/
+
+#ifndef TOKEN
+#define TOKEN "Your_Token"  // Put here your Ubidots TOKEN
+#endif
+
+#ifndef
+WEBHOOK_NAME = "my-particle-webhook"
+#endif
+
+Ubidots ubidots(TOKEN, UBI_PARTICLE);
+
+
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+
+//Put here your auxiliar functions
+
+
+/****************************************
+ * Main Functions
+ ****************************************/
+
+void setup() {
+  Serial.begin(115200);
+  //ubidots.setDebug(true);  // Uncomment this line for printing debug messages
+}
+
+void loop() {
+  float value1 = analogRead(A0);
+  float value2 = analogRead(A1);
+  float value3 = analogRead(A2);
+  ubidots.add("Variable_Name_One", value1);  // Change for your variable name
+  ubidots.add("Variable_Name_Two", value2);
+  ubidots.add("Variable_Name_Three", value3);
+
+  bool bufferSent = false;
+  bufferSent = ubidots.send(WEBHOOK_NAME, PUBLIC);  // Will use particle webhooks to send data
+
+  if(bufferSent){
+    // Do something if values were sent properly
+    Serial.println("Values sent by the device");
+  }
+
+  delay(5000);
+}
