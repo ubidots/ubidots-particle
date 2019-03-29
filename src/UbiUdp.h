aad8afad770ccf197893f798fd5f99babe7db0c3
jose garcia <jose.reyes@ubidots.com>
2019-03-29 14:56:53 -0500

    adding UDP destructor

--- src/UbiUdp.h
+++ src/UbiUdp.h
@@ -24,27 +24,32 @@ Developed and maintained by Jose Garcia for IoT Services Inc
 #ifndef _UbiUdp_H_
 #define _UbiUdp_H_
 
-#include "UbiProtocol.h"
 #include "Particle.h"
+#include "UbiProtocol.h"
+
+static UDP _client_udp_ubi;
 
 class UbiUDP : public UbiProtocol {
-  public:
-    UbiUDP(const char* host, const int port, const char* user_agent, const char* token);
-    bool sendData(const char* device_label, const char* device_name, char* payload, UbiFlags* flags);
-    float get(const char* device_label, const char* variable_label);
-    void setDebug(bool debug);
-  private:
-    const char *_host;
-    const char *_user_agent;
-    const char *_token;
-    int _port;
-    bool _debug = false;
-    UDP _client_udp_ubi;
-    int _timeout = 5000;
-    bool waitServerAnswer();
-    bool parseTCPAnswer(char* response);
-    void reconnect(const char * host, const int port);
-    IPAddress getServerIp();
+ public:
+  UbiUDP(const char* host, const int port, const char* user_agent,
+         const char* token);
+  bool sendData(const char* device_label, const char* device_name,
+                char* payload, UbiFlags* flags);
+  float get(const char* device_label, const char* variable_label);
+  void setDebug(bool debug);
+  ~UbiUDP();
+
+ private:
+  const char* _host;
+  const char* _user_agent;
+  const char* _token;
+  int _port;
+  bool _debug = false;
+  int _timeout = 5000;
+  bool waitServerAnswer();
+  bool parseTCPAnswer(char* response);
+  void reconnect(const char* host, const int port);
+  IPAddress getServerIp();
 };
 
 #endif
